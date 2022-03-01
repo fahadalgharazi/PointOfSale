58186ae9f4f759a88c674f90b5a64b56c5966c2e
Fahad Algharazi <fahad.algharazi@gmail.com>
2022-02-28 20:47:29 -0500

    first class set up

--- src/store/model/items/Item.scala
+++ src/store/model/items/Item.scala
@@ -1,20 +1,25 @@
 package store.model.items
 
-class Item {
+class Item(var itemDescription:String, var Itemprice:Double) {
 
   // TODO: Complete this class according to the features listed in the HW document
 
   def description(): String = {
-    ""
+    itemDescription
+  }
+
+  def setBasePrice(newPrice: Double): Unit ={
+    //changes the base price of the item
   }
 
   def price(): Double = {
-    0.0
+    Itemprice
+//    0.0
   }
 
 
 }
 
-class descriptionConstructor(var description:String, var price:Double){
-
-}
+//class descriptionConstructor(var description:String, var price:Double){
+//
+//}
